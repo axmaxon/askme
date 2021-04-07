855b6beacc91112991579535391b55276c3b48b3
axmaxon <axmaxon@gmail.com>
2021-04-07 21:22:35 +0500

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,53 @@
+require 'openssl'
 class User < ApplicationRecord
+
+  # Параметры работы модуля шифрования паролей
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions, :dependent => :destroy
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  # Добавляем виртуальный атрибут -пароль. Это поле будет в руби-объекте но не в БД
+  attr_accessor :password
+
+  # Валидируем атрибут пароль. Т.е rails перед сохранением проверят и это поле.
+  # Валидация будет происходить только при создании нового поль-ля (при выз. экшн create)
+  validates_presence_of :password, on: :create
+
+  # В объекте должно быть второе поле password_confirmation (для подтверждения пароля)
+  validates_confirmation_of :password
+
+  # Коллбэк чтобы зашифровать наши пароли ДО сохранения очередного пользователя в базу.
+  # (те что будут храниться в полях password_hash password_salt)
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # Создаем т.н. "соль" - рандомная строка усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      # Создаем хэш пароля - преобразуем пароль в длинную уникальную строку
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  # Служебный метод, преобразующий бинарную строку в 16-ричный формат, для удобства хранения
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  # Метод реализующий возможность залогиниться
+  def self.authenticate(email, password)
+    user = find_by(email: email) # сначала находим кандидата по email
+  #  Здесь сравнивается пароль преобразованный в password_hash
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
 end
