378d692c790d405e641f1d156447cdac64411efc
axmaxon <axmaxon@gmail.com>
2021-04-08 22:46:39 +0500

    add a private modifier to the encrypt_password method

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,17 +24,6 @@ class User < ApplicationRecord
   # (те что будут храниться в полях password_hash password_salt)
   before_save :encrypt_password
 
-  def encrypt_password
-    if self.password.present?
-      # Создаем т.н. "соль" - рандомная строка усложняющая задачу хакерам
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      # Создаем хэш пароля - преобразуем пароль в длинную уникальную строку
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
-
   # Служебный метод, преобразующий бинарную строку в 16-ричный формат, для удобства хранения
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -43,11 +32,24 @@ class User < ApplicationRecord
   # Метод реализующий возможность залогиниться
   def self.authenticate(email, password)
     user = find_by(email: email) # сначала находим кандидата по email
-  #  Здесь сравнивается пароль преобразованный в password_hash
+    # Здесь сравнивается пароль преобразованный в password_hash
     if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
       user
     else
       nil
+    end
+  end
+
+  private
+
+  def encrypt_password
+    if self.password.present?
+      # Создаем т.н. "соль" - рандомная строка усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      # Создаем хэш пароля - преобразуем пароль в длинную уникальную строку
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
     end
   end
 end
