707d0d96d6bfddcbf6d0051c71d87e7de0c8e893
axmaxon <axmaxon@gmail.com>
2021-04-11 01:14:56 +0500

    configure the webpack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Здравствуйте"
+    @user = User.new(
+      name: 'Michael',
+      username: 'blain',
+      avatar_url: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/bf74dda4-fdae-45af-8fe0-2e5fe5776566/280x420'
+    )
   end
 end
