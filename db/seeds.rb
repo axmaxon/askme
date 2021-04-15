ca87f95cbe0ed553ac9d01d31fb8d610a345d831
axmaxon <axmaxon@gmail.com>
2021-04-15 11:44:47 +0500

    add gem Faker and fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Создадим 30 юзеров с валидными данными, у каждого по 1-му вопросу
+30.times do |n|
+  name = Faker::Name.first_name
+  username = "#{Faker::Internet.username(specifier: name)}_#{n}"
+  user = User.new(
+    name: name,
+    username: username,
+    email: Faker::Internet.free_email(name: username),
+    password: '12346'
+  )
+  user.save!
+  question = Question.new(
+    user: user,
+    text: "Do you #{Faker::Verb.base}?"
+  )
+  question.save!
+end
