b58e720ff35cad4facc0ddf6e3459b77fc45a012
axmaxon <axmaxon@gmail.com>
2021-04-19 16:00:24 +0500

    add locale and question counters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,23 @@ class UsersController < ApplicationController
   end
 
   def show
+    # Ищем юзера, используя в кач-ве идентификатора используем значение id из params
+    @user = User.find params[:id]
+    # Достаем вопросы пользователя с помощью метода questions, который мы
+    # объявили в модели User (has_many :questions), у результата возврата этого
+    # метода вызываем метод order, который отсортирует вопросы по дате.
+    @questions = @user.questions.order(created_at: :desc)
 
+    # Для формы нового вопроса, которая есть у нас на странице пользователя,
+    # создаем болванку вопроса, вызывая метод build у результата вызова метода
+    # @user.questions.
+    @new_question = @user.questions.build
+
+    # Создаем три счётчика: для количества вопросов, отвеченных вопросов и
+    # неотвеченных вопросов
+    @questions_count = @questions.count
+    @answers_count = @questions.where.not(answer: nil).count
+    @unanswered_count = @questions_count - @answers_count
   end
 
   # Действие create будет отзываться при POST-запросе по адресу /users из формы
@@ -25,6 +41,12 @@ class UsersController < ApplicationController
     # Если юзер сохранен, перенаправляем запрос на root, который определн нами в routes
     if @user.save
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      # Если не удалось по какой-то причине сохранить пользователя, то рисуем
+      # (и это не редирект), страницу new с формой пользователя, который у нас
+      # лежит в переменной @user. В этом объекте
+      # содержатся ошибки валидации, которые выведет шаблон формы.
+      render 'new'
     end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Askme
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Укажем что по умаолчанию у нас русская локаль
+    config.i18n.default_locale = :ru
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
