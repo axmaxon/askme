3066076dd85fc330a1b6569f9f379d8aa3f7a184
axmaxon <axmaxon@gmail.com>
2021-04-20 12:54:48 +0500

    add user authentication and profile editing restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   # except - про то каких экшенов не будет в этом ресурсе/контроллере
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: [:destroy]
   resources :questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'show' => 'users#show'
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
