9ecda1d1389bde219cda35c69c1c1b4207d29ed9
axmaxon <axmaxon@gmail.com>
2021-04-30 11:21:20 +0500

    add: question authors

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
-
+  belongs_to :author, class_name: 'User', optional: true
   # Проверка на наличие собственно текста вопроса, соотв-е допустимому размеру.
   # Проверка на наличие атрибута user происходит автоматически, вследствие связи belongs_to
   validates :text, presence: true, length: { maximum: 255 }
