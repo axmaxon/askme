77ccb57394dda7a66e2e6685afba440b6b96b8de
axmaxon <axmaxon@gmail.com>
2021-07-07 16:22:44 +0500

    refactor: creating and updating hashtags

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,19 +8,17 @@ class Question < ApplicationRecord
   # Проверка на наличие атрибута user происходит автоматически, вследствие связи belongs_to
   validates :text, presence: true, length: { maximum: 255 }
 
-  after_commit :do_hashtag, on: [:create, :update]
+  after_commit :create_or_update_hashtags, on: [:create, :update]
 
   private
 
-  def do_hashtag
-    self.hashtags.clear
+  def create_or_update_hashtags
+    # Ищем хэштеги по тексту вопроса и ответа
+    unique_hashtags = ("#{text} #{answer.to_s}").scan(/#[[:word:]]+/).uniq
 
-    # Ищем хэштеги по тексту вопроса и по ответа
-    hashtags = (self.text + self.answer.to_s).scan(/#[[:word:]]+/)
-
-    hashtags.uniq.map do |hashtag|
-      tag = Hashtag.find_or_create_by(name: hashtag.downcase.delete('#'))
-      self.hashtags << tag
-    end
+    self.hashtags =
+      unique_hashtags.map do |unique_hashtag|
+        Hashtag.find_or_create_by(name: unique_hashtag.downcase.delete('#'))
+      end
   end
 end
