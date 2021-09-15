95b5d2f25d125af6e5a26c5151de305238195559
axmaxon <axmaxon@gmail.com>
2021-09-15 15:19:01 +0500

    add: filter for hashtags to HashtagsController

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,6 +1,6 @@
 class HashtagsController < ApplicationController
   def show
-    @hashtag = Hashtag.find_by(name: params[:name])
+    @hashtag = Hashtag.relevant.find_by!(name: params[:name])
     @questions = @hashtag.questions
   end
 end
